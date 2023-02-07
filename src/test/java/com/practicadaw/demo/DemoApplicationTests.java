8c3b5fc6719892e5415da677625398cf0cf50fc9
adri <alumno.1092944@ies-azarquiel.es>
2023-02-07 14:07:31 +0100

    Commit inicial

---
+++ src/test/java/com/practicadaw/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practicadaw.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
